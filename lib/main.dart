b126b49bbacc4a82a6fc158558350f7f75dada1b
TzVlad <129984940+TzVlad@users.noreply.github.com>
2024-10-08 23:53:24 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+    Map<String, dynamic> about = {
+        "name": "Vlad",
+        "age": "15",
+        "hobby": "Грати ігри, активне сидіння за компом, спорт",
+    };
+    writeKey(about);
+    namekey()
+    
+
+}
+
+void writeKey(Map<String, dynamic> about) {
+}
